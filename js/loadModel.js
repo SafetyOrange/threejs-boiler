ad8d0d93dc14e9bfba6035a8a4771b934ec7ca02
samsniderheld <samsniderheld@Deep-Thought.local>
2015-01-14 11:53:49 -0500

    update three version

--- js/loadModel.js
+++ js/loadModel.js
@@ -7,12 +7,13 @@ var loadModel =  {
 		var loader = new THREE.JSONLoader();
 	    loader.load( path, function(geometry, materials){
 	      	mesh = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial( materials ) );
-	      	mesh.postion={x:xpos, y:ypos, z:zpos};
-	      	mesh.scale={x:scale, y:scale, z:scale};
 	      	Config.scene.add(mesh);
+	      	mesh.postion={x:xpos, y:ypos, z:zpos};
+	      	mesh.scale.x = mesh.scale.y = mesh.scale.z = scale;
 	      	mesh.material.materials[0].side = 1;
 	    });
 	}
- 
+
+
 };
 
